c5e04323719ef9f3b44c26897788045388e37cce
zcy0024 <zcy0024@qq.com>
2024-10-31 11:01:04 +0800

    config: 博客配置更新

--- blog.config.js
+++ blog.config.js
@@ -26,8 +26,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '车前子', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个脱离了低级趣味的人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://gm.cloudns.ch', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '车前子', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://daokedao.cc', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'notion,blog,博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
