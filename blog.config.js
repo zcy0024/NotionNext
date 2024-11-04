e6869bbd691b65313da9e7a0d0cad70161cd3989
zcy0024 <zcy0024@qq.com>
2024-11-04 10:16:48 +0800

    config: welcome word update.

--- blog.config.js
+++ blog.config.js
@@ -20,12 +20,12 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '少无适俗韵，性本爱丘山。',
+    '海内存知己，天涯若比邻。',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '车前子', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个脱离了低级趣味的人', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '少无适俗韵，性本爱丘山。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://daokedao.cc', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'notion,blog,博客', // 网站关键词 英文逗号隔开
 
